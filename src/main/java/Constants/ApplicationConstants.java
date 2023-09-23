7a587271c8029764104d7fee840b9f1aeb35284e
pranjalesh <31514465+pranjalesh@users.noreply.github.com>
2023-09-23 17:13:47 -0400

    Added constants file

---
+++ src/main/java/Constants/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package Constants;
+
+/**
+ * This class is used to initialise all the constants that are going to be used throughout the code base.
+ *
+ * @version 1.0.0
+ */
+public class ApplicationConstants {
+    public static final String SRC_MAIN_RESOURCES = "src/main/resources";
+}
