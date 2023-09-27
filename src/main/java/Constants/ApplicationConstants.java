8390abf2bc6614278171f61d19029790756e10bc
yatishChutani <yatishchutani@gmail.com>
2023-09-27 18:05:53 -0400

    updated the ApplicationConstants file

--- src/main/java/Constants/ApplicationConstants.java
+++ src/main/java/Constants/ApplicationConstants.java
@@ -8,5 +8,9 @@ package Constants;
 public class ApplicationConstants {
     public static final String SRC_MAIN_RESOURCES = "src/main/resources";
     public static final String INVALID_COMMAND_ERROR_DEPLOY_ORDER = "Invalid command. Kindly provide command in Format of : deploy countryID <CountryName> <num> (until all reinforcements have been placed)";
+    public static final int CONSOLE_WIDTH = 80;
+    public static final String CONTROL_VALUE = "Control Value";
+    public static final String CONNECTIVITY = "Connections";
+    public static final String ARMIES = "Armies";
 
 }
