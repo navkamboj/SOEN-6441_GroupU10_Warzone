f639a9c48d65df3a88eca376dbd203e14cd222c4
Nihal_Galani <nihalgalani1240@gmail.com>
2023-09-28 19:49:19 -0400

    Update PlayerService file ApplicationConstants file

--- src/main/java/Services/PlayerService.java
+++ src/main/java/Services/PlayerService.java
@@ -23,15 +23,15 @@ public class PlayerService{
        /**
         *Method is used to check if a player name is exist or not.
         *
-        * @param p_allexistingplayerslist list of existing player
+        * @param p_allExistingPlayersList list of existing player
         * @param p_playerName name on which operation needs to be performed
         * @return boolean true if player name is unique otherwise return false if its not unique
         */
 
-       public boolean checkPlayerNameUniqueness(List<Player> p_allexistingplayerslist, String p_playerName){
+       public boolean checkPlayerNameUniqueness(List<Player> p_allExistingPlayersList, String p_playerName){
            boolean l_checkunique = true;
-           if(!CommonUtil.isEmptyCollection(p_allexistingplayerslist)){
-             for(Player l_player : p_allexistingplayerslist){
+           if(!CommonUtil.isEmptyCollection(p_allExistingPlayersList)){
+             for(Player l_player : p_allExistingPlayersList){
                 if(l_player.getPlayerName().equalsIgnoreCase(p_playerName)){
                    l_checkunique=true;
                    break;
@@ -153,7 +153,7 @@ public class PlayerService{
         List<Player> l_players = p_gameState.getD_playerList();
 
         for(int i = 0; i< l_players.size(); i++){
-            l_players.get(i).setD_color(ApplicationConstants.COLORS.get(i));
+            l_players.get(i).setD_color(ApplicationConstants.COLORSLIST.get(i));
         }
     }
 
